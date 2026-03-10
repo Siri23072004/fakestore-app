7de689edd35d32958685953e32d46b784dd2c016
Siri <sirianil2004@gmail.com>
2026-03-10 21:35:30 +0530

    feat: create reusable ProductCard component with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Home from './pages/Home';
 
 function App() {
-  console.log("App is rendering!"); // This will show in Console
+  console.log("App is rendering!"); 
   return (
     <div>
       <Home />
